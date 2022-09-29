88195a92bff658b88d98e93d39bf96a241239506
SeanPiedmonte <seanpiedmonte@gmail.com>
2022-09-29 11:44:47 -0400

    Begin BFS method and created the graph

--- bfs.py
+++ bfs.py
@@ -1,4 +1,16 @@
-graph = {
-    "s" : [a,b,c],
-    
+test_graph = {
+    "s" : ["a","b","c"],
+    "a" : ["d", "e"],
+    "b" : ["g"],
+    "c" : ["f"],
+    "d" : ["h"],
+    "e" : ["g"],
+    "f" : ["g"],
+    "h" : []
 }
+
+def bfs(graph, goal):
+    frontier_list = []
+    for key in graph:
+        frontier_list.append(key)
+        
