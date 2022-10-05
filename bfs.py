f4e8f529801f6589d4359c3c4f4fa4f2e4638869
Sean Piedmonte <seanpiedmonte@gmail.com>
2022-10-04 20:40:35 -0400

    implemented bfs search that returns the traversal

--- bfs.py
+++ bfs.py
@@ -1,3 +1,6 @@
+import queue
+
+
 test_graph = {
     "s" : ["a","b","c"],
     "a" : ["d", "e"],
@@ -9,8 +12,35 @@ test_graph = {
     "h" : []
 }
 
-def bfs(graph, goal):
-    frontier_list = []
-    for key in graph:
-        frontier_list.append(key)
+
+def bfs(start, goal, graph):
+    visited = []
+    queue = []
+    path = []
+
+    path.append(start)
+    queue.append(start)
+    visited.append(start)
+
+    if start == goal:
+        path.append(goal)
+        return path
+
+    while queue:
+        node = queue.pop(0)
+        if node == goal:
+            return visited
         
+        neighbors = graph[node]
+        for neighbor in neighbors:
+            if neighbor == goal:
+                visited.append(neighbor)
+                return visited
+
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+    return visited
+
+path = bfs("s", "g", test_graph)
+print(path)
